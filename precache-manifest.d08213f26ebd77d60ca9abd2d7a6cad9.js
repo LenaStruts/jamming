7a8f8a2f83495f7207cc2c4ecca3b293a0c59b73
LenaStruts <lena19struts@gmail.com>
2020-09-14 15:51:49 +0200

    Updates

---
+++ precache-manifest.d08213f26ebd77d60ca9abd2d7a6cad9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc2370ad210332ec85926f3ddf19234",
+    "url": "/jamming/index.html"
+  },
+  {
+    "revision": "6dd8a5eb2d75dbd58492",
+    "url": "/jamming/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2489ef1a940881a47f25",
+    "url": "/jamming/static/js/2.093c662c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jamming/static/js/2.093c662c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd8a5eb2d75dbd58492",
+    "url": "/jamming/static/js/main.4da058fa.chunk.js"
+  },
+  {
+    "revision": "d466fbb093b3649303f8",
+    "url": "/jamming/static/js/runtime-main.6d782cb8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jamming/static/media/logo.5d5d9eef.svg"
+  }
+]);
